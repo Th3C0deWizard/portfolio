33c0bfad0f2b5c28c7ff186b9682d2ae556b15c8
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2024-02-27 22:03:20 -0500

    almost done

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+// autoscroll skills-scroller.
+
+const container = document.querySelector(".skills-scroller");
+const scrollSpeed = 100;
+
+function scrollContent(direction = 1) {
+  if (container.scrollLeft < container.scrollWidth - container.clientWidth) {
+    container.scrollLeft += direction;
+  } else {
+    container.scrollLeft = 0;
+  }
+}
+
+var scrollInterval = setInterval(scrollContent, 1000 / scrollSpeed);
+
+// autoscroll projects-container
+
+const projectscontainer = document.querySelector(".projects-container");
+const projectsscrollinterval = 7000;
+var userScrolling = false;
+var timeout;
+
+function scrollContenty(direction = 1) {
+  if (
+    projectscontainer.scrollTop <
+      projectscontainer.scrollHeight - projectscontainer.clientHeight &&
+    estaEnViewport(projectscontainer)
+  ) {
+    projectscontainer.scrollTop += direction * 400;
+  } else {
+    projectscontainer.scrollTop = 0;
+  }
+}
+
+projectscontainer.addEventListener("scroll", () => {
+  userScrolling = true;
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    userScrolling = false;
+  }, 500);
+});
+
+var projectsscrollInterval = setInterval(() => {
+  if (!userScrolling) scrollContenty();
+}, projectsscrollinterval);
+
+// addEventListener to sidenav
+
+function estaEnViewport(elemento) {
+  var rect = elemento.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const mainContainer = document.querySelector("main");
+const as = document.querySelectorAll(".sidenav a");
+mainContainer.addEventListener("scroll", () => {
+  as.forEach((element) => {
+    const section = document.querySelector(element.href.match(/#(.*)/));
+    console.log("section: ", section.offsetTop);
+    console.log("window: ", window.scrollY);
+    console.log(
+      "window + clientHeight: ",
+      window.scrollY + section.clientHeight,
+    );
+    if (estaEnViewport(section)) {
+      element.dataset.current = true;
+      console.log(":D");
+    } else {
+      element.dataset.current = false;
+      console.log(":C");
+    }
+  });
+});
